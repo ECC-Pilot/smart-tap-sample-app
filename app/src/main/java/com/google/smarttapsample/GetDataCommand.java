1addbd8437e4b8f18f9f33cb2dc1c44c72925682
Alexander Loechel <Alexander.Loechel@lmu.de>
2023-08-31 01:23:20 +0200

    Code comment cleanup

--- app/src/main/java/com/google/smarttapsample/GetDataCommand.java
+++ app/src/main/java/com/google/smarttapsample/GetDataCommand.java
@@ -32,11 +32,10 @@ class GetDataCommand {
       (byte) 0x00
   };
 
-  // Hardcoded for loyalty passes
+
   // https://developers.google.com/wallet/smart-tap/reference/apdu-commands/get-data#service_type_byte
-//  private static final byte SERVICE_TYPE = 0x03;
-  private static final byte SERVICE_TYPE = 0x00;    // this one accepts all card types
-//  private static final byte SERVICE_TYPE = 0x03;
+  private static final byte SERVICE_TYPE = 0x00;  // this one accepts all card types
+//  private static final byte SERVICE_TYPE = 0x03;  // Hardcoded for loyalty passes
 
   private NdefRecord serviceRequestRecord;
 
